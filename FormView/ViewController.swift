ff2e792ad0a68ca57ba43a53fa85a2b15805ec30
JacobHanna18 <jacob18111998@gmail.com>
2020-09-29 18:03:46 +0300

    Initial Commit

---
+++ FormView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FormView
+//
+//  Created by Jacob Hanna on 29/09/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
